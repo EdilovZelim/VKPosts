49b0b8f5f210cbecd57a37c1c1bca4806c567d62
ZelimhanEdilov <savachoc@mail.ru>
2019-09-19 00:33:55 +0300

    VkPosts

--- VKPosts/Newsfeed/TitleView/TitleView.swift
+++ VKPosts/Newsfeed/TitleView/TitleView.swift
@@ -62,6 +62,6 @@ class TitleView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implementedd")
     }
 }
